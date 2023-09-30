0132eef3d7ce4ffbcd08aa05f5fcf8425c3ddf4b
Violet Bit Kitten <140219752+VioletBitKitten@users.noreply.github.com>
2023-09-30 13:53:41 -0400

    Delete the site and run the server in development mode.

--- run.bat
+++ run.bat
@@ -1,1 +1,13 @@
+@ECHO OFF
+REM Build and serve the Blog using Jekyll.
+
+REM Delete the site files to rebuild from scratch.
+IF EXIST "_site" (
+    ECHO Deleting the site files.
+    DEL /Q _site
+)
+
+REM Build the site and start the server.
+ECHO Building the site and serving in Development mode.
+SET JEKYLL_ENV=development
 bundle exec jekyll serve --watch
